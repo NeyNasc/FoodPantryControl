096fc8d1bcdc7bee97a77c6f67879f687322c3bd
Claudiney <claudineydelima@gmail.com>
2020-04-08 21:43:18 -0300

    Commit Incial

---
+++ FoodPantryControl/FoodPantryControl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FoodPantryControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

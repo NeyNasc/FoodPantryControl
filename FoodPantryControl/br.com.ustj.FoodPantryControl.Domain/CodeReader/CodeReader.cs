5de29e40bf9b8ee89c91df7b7eb2d1808eb3f9fd
Claudiney <claudineydelima@gmail.com>
2020-04-08 22:16:29 -0300

    Adicionando testes e domain

---
+++ FoodPantryControl/br.com.ustj.FoodPantryControl.Domain/CodeReader/CodeReader.cs
@@ -0,0 +1,12 @@
+﻿namespace br.com.ustj.FoodPantryControl.Domain.CodeReader
+{
+    public class CodeReader
+    {
+        public string BarCodeNumber { get; set; }
+
+        public CodeReader(string barCodeNumber)
+        {
+            this.BarCodeNumber = barCodeNumber;
+        }
+    }
+}
---
+++ FoodPantryControl/br.com.ustj.FoodPantryControl.Tests/Case/Domain/CodeReaderTest.cs
@@ -0,0 +1,17 @@
+﻿using br.com.ustj.FoodPantryControl.Domain.CodeReader;
+using System;
+using Xunit;
+
+namespace br.com.ustj.FoodPantryControl.Tests.Case.Domain
+{
+    public class CodeReaderTest
+    {
+
+        [Fact]
+        public void ShouldCreateValid()
+        {
+            var model = new CodeReader("2222222111");
+            Assert.True(!String.IsNullOrEmpty(model.BarCodeNumber));
+        }
+    }
+}

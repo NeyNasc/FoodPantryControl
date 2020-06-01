600306cc68460b6d979cad181c5a5e6526269192
Claudiney <claudineydelima@gmail.com>
2020-05-31 23:27:37 -0300

    Add Log

---
+++ src/br.com.ustj.FoodPantryControl/br.com.ustj.FoodPantryControl.Domain/Log/Log.cs
@@ -0,0 +1,28 @@
+﻿using br.com.ustj.FoodPantryControl.Domain.Enum;
+using System;
+
+namespace br.com.ustj.FoodPantryControl.Domain.Log
+{
+    public class Log: Entity
+    {
+        public TypeLog TypeLog { get; private set; }
+        public DateTime LogDate { get; private set; }
+        public string Message { get; private set; }
+        public string Stacktrace { get; private set; }
+
+        public Log(TypeLog typeLog, DateTime logDate, string message)
+        {
+            TypeLog = typeLog;
+            LogDate = logDate;
+            Message = message;
+        }
+
+        public Log(TypeLog typeLog, DateTime logDate, string message, string stacktrace)
+        {
+            TypeLog = typeLog;
+            LogDate = logDate;
+            Message = message;
+            Stacktrace = stacktrace;
+        }
+    }
+}
